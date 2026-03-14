91e83875254a8b9851debd5e0ee18b68bedd6db8
Abdubakiyev <abdubakiyevabdulloh798@gmail.com>
2026-03-14 10:49:48 +0500

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@
 import { Module } from '@nestjs/common';
 import { QarzdorModule } from './qarzdor/qarzdor.module';
 import { PrismaModule } from './core/prisma.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [
     PrismaModule,
     QarzdorModule,
   ],
+  controllers: [AppController],
 })
 export class AppModule {}
